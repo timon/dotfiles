c6c78b96a7001582682eafc0287f69ecbee415aa
Artem Ignatyev <artem.ignatyev@eurochem.ru>
2015-12-11 17:53:37 +0300

    Add installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+MYDIR=`cd $(dirname $0); pwd`
+
+update_submodules() {
+	echo "Updating submodules in ${MYDIR}"
+	pushd "$MYDIR"
+	git submodule update --init
+	popd
+}
+
+symlink_files() {
+	echo "Setting up dotfiles from ${MYDIR}"
+	for file in .bashrc .profile .vim .vimrc
+	do
+		ln -nsf "${MYDIR}/${file}" ~
+	done
+}
+
+run_vundle() {
+	vim -u ~/.vim/bundles.vim +BundleInstall +qall
+}
+
+update_submodules
+symlink_files
+run_vundle
