b0a10391898af98bd7c9138d2bde2145fbaa182d
Artem Ignatyev <artem.ignatyev@eurochem.ru>
2018-12-12 15:23:48 +0300

    Add rubocop.yml stub

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ update_submodules() {
 
 symlink_files() {
 	echo "Setting up dotfiles from ${MYDIR}"
-	for file in .bashrc .inputrc .profile .vim .vimrc
+	for file in .bashrc .inputrc .profile .ruobcop.yml .vim .vimrc
 	do
 		ln -nsf "${MYDIR}/${file}" ~
 	done
